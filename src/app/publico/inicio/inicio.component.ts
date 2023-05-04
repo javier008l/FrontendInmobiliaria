6752deeb34c7384dcdb48536a3e9d18df7111ef8
leon55490 <leon55490@gmail.com>
2023-05-04 18:33:10 -0500

    Página Inicio Sesion Activa

--- src/app/publico/inicio/inicio.component.ts
+++ src/app/publico/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsuarioValidadoModel } from 'src/app/modelos/usuario.valido.model';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  constructor(
+    private servicioSeguridad: SeguridadService
+  ){
 
+  }
+
+  sesionAtiva: boolean = false;
+
+  ngOnInit(){
+    this.ValidarSesion();
+  }
+
+  ValidarSesion(){
+    this.servicioSeguridad.ObtenerDatosSesion().subscribe({
+      next: (datos: UsuarioValidadoModel)=>{
+        if(datos.token != ""){
+          this.sesionAtiva = true;
+        }else{
+          this.sesionAtiva = false;
+        }
+      },
+      error:(err: any)=>{
+
+      }
+    })
+  }
 }
