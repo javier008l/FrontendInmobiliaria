1aa75a30c2b15b336f3d3916f934c4daa519c18f
Eliana <eliana04112001@gmail.com>
2023-05-10 23:34:16 -0500

    Listado de inmuebles en el inicio del sistema

---
+++ src/app/servicios/parametros.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ConfiguracionRutasBackend } from '../config/configuracion.rutas.backend';
+import { InmuebleModel } from '../modelos/inmueble.model';
+import { Observable } from 'rxjs';
+import { ConfiguracionPaginacion } from '../config/configuracion.paginacion';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParametrosService {
+  urlBase: string = ConfiguracionRutasBackend.urlLogica;
+  
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Listado de inmuebles
+   * @returns
+   */
+  listarRegistros():Observable<InmuebleModel[]>{
+   return this.http.get<InmuebleModel[]>(`${this.urlBase}inmueble?filter={"limit":${ConfiguracionPaginacion.registrosPorPagina}}`);
+  }
+}
