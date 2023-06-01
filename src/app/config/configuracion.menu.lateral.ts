9bf91b72a29220636a69527042d2f9e30e12e23c
Eliana <eliana04112001@gmail.com>
2023-06-01 16:47:59 -0500

    Menu Lateral del Asesor

--- src/app/config/configuracion.menu.lateral.ts
+++ src/app/config/configuracion.menu.lateral.ts
@@ -1,5 +1,6 @@
 export namespace ConfiguracionMenuLateral{
-    export const listarMenus=[{
+    export const listarMenus=[
+        {
         id: "640f96d73c37555c309f9455",
         ruta: "/seguridad/usuario-listar",
         texto: "Usuarios"
@@ -9,6 +10,8 @@ export namespace ConfiguracionMenuLateral{
         ruta: "/seguridad/asesor-listar",
         texto: "Asesor"
     },
+
+    // MENU LATERAL DEL CLIENTE
     {
         id: "641e21713ae85517e8ef09c7",
         ruta: "/parametros/inmueble-listar",
@@ -23,7 +26,29 @@ export namespace ConfiguracionMenuLateral{
         id: "6479040ef5616802009c2bac",
         ruta: "/seguridad/formulario-contacto-cliente",
         texto: "Contáctanos"
-    }
+    },
+
+    // MENU LATERAL DEL ASESOR
+    {
+        id: "64790cc3f5616802009c2bb4",
+        ruta: "/seguridad/",
+        texto: "Solicitudes a estudiar"
+    },
+    {
+        id: "64790d8df5616802009c2bb6",
+        ruta: "/seguridad/",
+        texto: "Inmuebles a cargo"
+    },
+    {
+        id: "64790da4f5616802009c2bb7",
+        ruta: "/seguridad/",
+        texto: "Crear un inmueble"
+    },
+    {
+        id: "647909f2f5616802009c2bb2",
+        ruta: "/seguridad/formulario-contacto-asesor",
+        texto: "Contactar un cliente"
+    },
 
 ];
 }
