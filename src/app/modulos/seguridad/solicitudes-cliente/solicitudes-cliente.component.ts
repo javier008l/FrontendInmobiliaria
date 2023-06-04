09cb960d74d42df0c6d8122a082b21a13380ca51
David Rincon Toro <leon55490@gmail.com>
2023-06-03 23:05:36 -0500

    solicitudes cliente

--- src/app/modulos/seguridad/solicitudes-cliente/solicitudes-cliente.component.ts
+++ src/app/modulos/seguridad/solicitudes-cliente/solicitudes-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SolicitudModel } from 'src/app/modelos/solicitud.model';
 import { ParametrosService } from 'src/app/servicios/parametros.service';
 
@@ -8,11 +9,26 @@ import { ParametrosService } from 'src/app/servicios/parametros.service';
   styleUrls: ['./solicitudes-cliente.component.css']
 })
 export class SolicitudesClienteComponent {
+
+  fGroup: FormGroup = new FormGroup({});
   listaSolicitudes: SolicitudModel[] = [];
   servicioSeguridad: any;
   sesionActiva: boolean | undefined;
 
-  constructor(private servicioParametrizacion: ParametrosService) { }
+  constructor(private servicioParametrizacion: ParametrosService, private fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.ConstruirFormulario();
+  }
+
+
+  ConstruirFormulario() {
+    this.fGroup = this.fb.group({
+      contrato: ['', [Validators.required, Validators.minLength(2)]],
+    });
+  }
 
   obtenerEstadoSolicitud(estadoId: number): string {
     if (estadoId === 1) {
@@ -46,11 +62,22 @@ export class SolicitudesClienteComponent {
   }
 
   contrato() {
+    alert("Subir el contrato firmado a google drive e introducir el link en subir contrato")
     window.open('https://drive.google.com/file/d/1FOJc2CWwjKx6jC9ZC-zw6GivNq89HOlB/view?usp=sharing', '_blank');
+  }
+
+  subirContrato() {
+    let contrato = this.ObtenerFormGroup["contrato"].value;
+    console.log(contrato)
+
   }
 
   coDeudor() {
     window.open('https://drive.google.com/file/d/1PU66vg1BWsF_w9bvfpAwXRRDAH7eQfG8/view?usp=sharing', '_blank');
+  }
+
+  get ObtenerFormGroup() {
+    return this.fGroup.controls;
   }
 }
 
